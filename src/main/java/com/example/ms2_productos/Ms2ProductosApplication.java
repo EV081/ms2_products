0ff967b6420fb64e086fb9c13d25fb20d466eae8
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-09-27 14:39:27 -0500

    feat: update

---
+++ src/main/java/com/example/ms2_productos/Ms2ProductosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms2_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms2ProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms2ProductosApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ms2_productos/domain/Categoria.java
@@ -0,0 +1,29 @@
+package com.example.ms2_productos.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCategoria;
+
+    private String nombreCategoria;
+    private String descripcionCategoria;
+
+    @OneToMany(mappedBy = "categoria")
+    @JsonManagedReference
+    private List<Producto> productos = new ArrayList<>();
+
+}
