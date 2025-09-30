53feae2383da9b37219a24030a881a1fc63b6c5c
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-09-29 20:16:37 -0500

    feat: update 4

---
+++ src/main/java/com/example/ms2_productos/application/Healthcheck.java
@@ -0,0 +1,17 @@
+package com.example.ms2_productos.application;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
+public class Healthcheck {
+
+    @GetMapping
+    public ResponseEntity<String> check() {
+        return ResponseEntity.ok("OK");
+    }
+}
+
